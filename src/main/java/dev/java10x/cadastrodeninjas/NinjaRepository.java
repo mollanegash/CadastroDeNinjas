8760d47970f2d19629f91d458557b9079485a003
mollanegash <mdraya3@gmail.com>
2025-04-22 16:11:50 +0300

    Revert "Add Ninja CRUD with H2 database support, update README with testing instructions, and configure Spring Boot application."
    This reverts commit 4f504f838006b706298fd2e91ea558e78b7a7f24.

--- src/main/java/dev/java10x/cadastrodeninjas/NinjaRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.java10x.cadastrodeninjas;
-
-import dev.java10x.cadastrodeninjas.model.Ninja;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NinjaRepository extends JpaRepository<Ninja, Long> {
-}
